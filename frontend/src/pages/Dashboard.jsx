010e255d0f98ec993b0961ba25c6d673ba05c0e9
FinityM <B00125089@mytudublin.ie>
2022-04-07 15:50:12 +0100

    Added comments to backend
    Comments added for backend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,83 +1,69 @@
-import {useEffect} from 'react'
-import {useNavigate} from 'react-router-dom'
-import 'react-toastify/dist/ReactToastify.css'
-import {useSelector, useDispatch} from 'react-redux'
-import GoalForm from '../components/GoalForm'
-import GoalItem from '../components/GoalItem'
-import Spinner from '../components/Spinner'
-import Timer from '../components/PomodoroTimer'
-import {getGoals, reset} from '../features/goals/goalSlice'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { useSelector, useDispatch } from "react-redux";
+import GoalForm from "../components/GoalForm";
+import GoalItem from "../components/GoalItem";
+import Spinner from "../components/Spinner";
+import Timer from "../components/PomodoroTimer";
+import { getGoals, reset } from "../features/goals/goalSlice";
 
 function Dashboard() {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const {user} = useSelector((state) => state.auth)
-    const {goals, isLoading, isError, message} = useSelector(
-        (state) => state.goals
+  const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
 
-        
-
-
-    )
-
-    useEffect(() => {
-        if (isError) {
-            console.log(message)
-        }
-
-        if (!user) {
-            navigate('/login')
-        }
-
-        dispatch(getGoals())
-
-        if (!isError) {
-            dispatch(reset)
-        }
-
-        return () => {
-            dispatch(reset())
-        }
-    }, [user, navigate, isError, message, dispatch])
-
-    if (isLoading) {
-        return <Spinner/>
+  useEffect(() => {
+    if (isError) {
+      console.log(message);
     }
 
-    return (
-        <>
+    if (!user) {
+      navigate("/login");
+    }
 
-            <section className='heading'>
-                <h1>Welcome {user && user.name}</h1>
-            </section>
+    dispatch(getGoals());
 
+    if (!isError) {
+      dispatch(reset);
+    }
 
-            <GoalForm/>
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
 
-            <section className='content'>
-                {goals.length > 0 ? (
-                    <div className='goals'>
-                        {goals.map((goal) => (
-                            <GoalItem key={goal._id} goal={goal}/>
-                        ))}
-                    </div>
-                ) : (
-                    <h3>You have not set any goals</h3>
-                )}
-            </section>
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-            <Timer/>
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+      </section>
 
-        </>
+      <GoalForm />
 
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
 
-
-
-    )
-
-
-
+      <Timer />
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
