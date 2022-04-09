423f4a530e1d000067a6983e973f8d4a1cc1cb54
FinityM <B00125089@mytudublin.ie>
2022-04-09 22:07:55 +0100

    Removed old header logic

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -175,33 +175,6 @@ function Header() {
       </Container>
     </AppBar>
 
-    //<header className="header">
-    //     <div className="logo">
-    //         <Link to="/">W.I.P</Link>
-    //     </div>
-    //     <ul>
-    //         {user ? (
-    //             <li>
-    //                 <button className="btn" onClick={onLogout}>
-    //                     <FaSignOutAlt/> Logout
-    //                 </button>
-    //             </li>
-    //         ) : (
-    //             <>
-    //                 <li>
-    //                     <Link to="/login">
-    //                         <FaSignInAlt/> Login
-    //                     </Link>
-    //                 </li>
-    //                 <li>
-    //                     <Link to="/register">
-    //                         <FaUser/> Register
-    //                     </Link>
-    //                 </li>
-    //             </>
-    //         )}
-    //     </ul>
-    //</header>
   );
 }
 
