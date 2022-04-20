572118560e9b76b0f300415af244eed1f9679a27
FinityM <B00125089@mytudublin.ie>
2022-04-20 16:49:44 +0100

    Update GoalItem.jsx
    Formatted goalitem file

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,13 +1,13 @@
-import {useDispatch} from "react-redux";
-import {deleteGoal, updateGoal} from "../features/goals/goalSlice";
+import { useDispatch } from "react-redux";
+import { deleteGoal, updateGoal } from "../features/goals/goalSlice";
 import "../index.css";
 
-function GoalItem({goal}) {
+function GoalItem({ goal }) {
     const dispatch = useDispatch();
 
     return (
         <div className="goal">
-            
+
             <div>{new Date(goal.createdAt).toLocaleString("en-US")}</div>
 
             <h2>{goal.text}</h2>
@@ -15,10 +15,11 @@ function GoalItem({goal}) {
             <button onClick={() => dispatch(deleteGoal(goal._id))} className="close">
                 X
             </button>
-{/* 
+            {/* 
             <button onClick={() => dispatch(updateGoal(goal._id, goal.text))} className="edit">
                 Edit
-            </button> */}
+            </button>
+             */}
         </div>
     );
 }
